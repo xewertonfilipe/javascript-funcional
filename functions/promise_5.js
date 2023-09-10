b2364a79643af0043c44ec6f8aecc45b33636bad
Ewerton Filipe <xewertonfilipe@gmail.com>
2023-09-10 19:33:55 -0300

    feat(javascript-functional): promise_05

---
+++ functions/promise_5.js
@@ -0,0 +1,24 @@
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        try {
+            con.log('Temp')
+            if (Math.random() < chanceErro) {
+                reject('Ocorreu um error!')
+            } else {
+                resolve(valor)
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.5)
+    .then(v => `Valor: ${v}`)
+    .then(
+        v => console.log(v),
+        err => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(() => console.log('Quase Fim!'))
+    .catch(err => console.log(`Erro Geral: ${err}`))
+    .then(() => console.log('Fim'))
