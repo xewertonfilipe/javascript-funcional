8ef10f57ab5376590e4a57fe849dd8a95d15954d
Ewerton Filipe <xewertonfilipe@gmail.com>
2023-09-29 02:10:18 -0300

    feat(project#01): added index

---
+++ project#01/index.js
@@ -0,0 +1,10 @@
+const fn = require('./functions')
+
+fn.searchFiles('/dada/subtitles/')
+    .then(fn.readFiles)
+    .then(fn.countsWords)
+    .then(fn.orderWords)
+    .then(fn.createOutputFile)
+    .catch(console.log)
+
+
