7ca7c2531d4cd40b9d8fd869488f087f81026b40
Ewerton Filipe <xewertonfilipe@gmail.com>
2024-01-03 23:33:15 -0300

    feat(functions): added teacher code

---
+++ project#01/funcoes.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+
+
+function lerDiretorio(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            let arquivos = fs.readdirSync(caminho)
+            arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
+            resolve(arquivos)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+function lerArquivo(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+            resolve(conteudo.toString())
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+function lerArquivos(caminhos) {
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
+function elementosTerminadosCom(array, padraoTextual) {
+    return array.filter(el => el.endsWith(padraoTextual))
+}
+
+function removerSeVazio(array) {
+    return array.filter(el => el.trim())
+}
+
+function removerSeIncluir(array, padraoTextual) {
+    return array.filter(el => !el.includes(padraoTextual))
+}
+
+function removerSeApenasNumero(array) {
+    return array.filter(el => {
+        const num = parseInt(el.trim())
+        return num !== num
+    })
+}
+
+module.exports = {
+    lerDiretorio,
+    elementosTerminadosCom,
+    lerArquivo,
+    lerArquivos,
+    removerSeVazio,
+    removerSeIncluir,
+    removerSeApenasNumero
+}
