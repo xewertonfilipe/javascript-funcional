53449b5a1d8f7b18bdc22941ba5d1c52aa661f81
Ewerton Filipe <xewertonfilipe@gmail.com>
2024-04-18 00:11:52 -0300

    feat(pure-function): added immutability standard

--- project#01/funcoes.js
+++ project#01/funcoes.js
@@ -5,11 +5,10 @@ const path = require('path')
 function lerDiretorio(caminho) {
     return new Promise((resolve, reject) => {
         try {
-            const arquivos = fs.readdirSync(caminho)
-            const arquivosCompletos = arquivos.map(arquivo => {
+            const arquivos = fs.readdirSync(caminho).map(arquivo => {
                 return path.join(caminho, arquivo)
             })
-            resolve(arquivosCompletos)
+            resolve(arquivos)
         } catch (e) {
             reject(e)
         }
@@ -87,7 +86,7 @@ function ordernarPorAtribNumerico(attr, ordem = 'asc') {
     return function (array) {
         const asc = (o1, o2) => o1[attr] - o2[attr]
         const desc = (o1, o2) => o2[attr] - o1[attr]
-        return array.sort(ordem === 'asc' ? asc : desc)
+        return [...array].sort(ordem === 'asc' ? asc : desc)
     }
 }
 
